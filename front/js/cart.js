942259b7339071d751b3a32d6d142f4278f6cb73
Sauvage Florent <florentsauvage@outlook.fr>
2022-04-26 15:16:51 +0200

    Travail page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,33 +1,26 @@
 //Récuperation du produit dans le panier
+let productLocalStorage = JSON.parse(localStorage.getItem("Product"));
 
-let addProduct = JSON.parse(localStorage.getItem("Product"));
 
-const panierDisplay = async () => {
-    if(addProduct) {
-        await addProduct;
-        console.log(addProduct);
-    }
-}
-panierDisplay();
-
+//Intégration HTML + des données produit sur la page
 const parser = new DOMParser();
 let productSection = document.getElementById("cart__items");
-for (i = 0; i < addProduct.length; i++) {
+for (i = 0; i < productLocalStorage.length; i++) {
     let productItem = `
     <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
         <div class="cart__item__img">
-            <img src="${addProduct.imageUrl}" loading="lazy" alt="${addProduct.altTxt}">
+            <img src="${productLocalStorage[i].ImgSrc}" loading="lazy" alt="${productLocalStorage[i].ImgAlt}">
         </div>
         <div class="cart__item__content">
             <div class="cart__item__content__description">
-            <h2>${addProduct.name}</h2>
-            <p></p>
-            <p>${addProduct.price}</p>
+            <h2>${productLocalStorage[i].Name}</h2>
+            <p>${productLocalStorage[i].Color}</p>
+            <p>${productLocalStorage[i].Qty * productLocalStorage[i].Price} €</p>
             </div>
             <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
-                    <p>${addProduct.quantity}</p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">${addProduct.quantity * addProduct.price}
+                    <p>Qté : </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productLocalStorage[i].Qty}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                     <p class="deleteItem">Supprimer</p>
@@ -35,7 +28,60 @@ for (i = 0; i < addProduct.length; i++) {
             </div>
         </div>
     </article>`;
-    console.log()
     const htmlString = parser.parseFromString(productItem,"text/html");
         productSection.appendChild(htmlString.body.firstChild);
 }
+
+
+
+//------------------------------Calcul du prix total------------------------------//
+
+//Déclaration de variable pour mettre les prix des produits
+let totalCalculPrice = [];
+
+//Récupération des prix dans le panier
+for (let i = 0; i < productLocalStorage.length; i++){
+  let priceProductCart = productLocalStorage[i].Price;
+  totalCalculPrice.push(priceProductCart)
+}
+
+//Aditionner les prix dans la variable
+const reducerPrice = (accumulator, currentValue) => accumulator + currentValue;
+const totalPrice = totalCalculPrice.reduce(reducerPrice,0);
+document.querySelector('#totalPrice').innerHTML = totalPrice;
+
+
+//------------------------------Calcul de la quantité total------------------------------//
+
+//Déclaration de variable pour mettre la quantité des produits
+let totalCalculQty = [];
+
+//Récupération des quantités dans le panier
+for (let i = 0; i < productLocalStorage.length; i++){
+  let quantityProductCart = productLocalStorage[i].Qty;
+  totalCalculQty.push(quantityProductCart)
+}
+
+//Aditionner les quantités dans la variable
+const reducerQty = (accumulator, currentValue) => accumulator + currentValue;
+const totalQuantity = eval(totalCalculQty.join("+"));
+document.getElementById("totalQuantity").innerHTML = totalQuantity;
+
+
+/* Suppression d'items
+
+let deleteProduct = document.querySelectorAll(".deleteItem");
+console.log(deleteProduct)
+
+for (let i = 0; i < deleteProduct[i].length; i++){
+  deleteProduct[i].addEventListener("click", (event) =>{
+    event.preventDefault();
+
+    let deleteIdSelect = productLocalStorage[i].Id;
+    console.log(deleteIdSelect)
+
+
+    //productLocalStorage = productLocalStorage.filter( el => el.)
+    
+  }
+}*/
--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,9 @@
 //On récupére l'ID du produit dans le lien
-
 let url = new URL(window.location.href);
 let productId = url.searchParams.get("id");
 console.log(productId);
 
 //Requête vers l'API pour récupérer les données du produit.
-
 const products = "http://localhost:3000/api/products/";
 
 async function getArticle(id) {
@@ -21,7 +19,6 @@ async function getArticle(id) {
 }
 
 //Intégration des données reçu sur la fiche produit
-
 async function displayArticle(productId) {
   const article = await getArticle(productId);
 
@@ -45,7 +42,6 @@ async function displayArticle(productId) {
   console.log(article.description)
 
   //Boucle pour les couleurs disponibles du produit
-
   for (color of article.colors) {
     const productColors = document.createElement("option");
     document.querySelector("#colors").appendChild(productColors);
@@ -59,23 +55,22 @@ async function displayArticle(productId) {
 displayArticle(productId);
 
 function addToCart() {
+  
   //Définition des champs à renseigner
-
   const addBtn = document.getElementById("addToCart");
   const quantity = document.getElementById("quantity");
   const color = document.getElementById("colors");
 
   // Au clic, l'évènement s'effectue si les champs sont renseignés
-
   addBtn.addEventListener("click", () => {
     if (color.value == "" || quantity.value == 0 || quantity.value > 100) {
       
       //Alerte l'utilisateur si les conditions ne sont pas remplisse
-      
       alert ('Veuillez renseigner une quantité comprise entre 1 et 100 et une couleur')
       return;
     }
-      //Si les champs sont renseignés : stockage des données dans des variables
+      
+    //Si les champs sont renseignés : stockage des données dans des variables
     else {
       let userProductId = productId;
       let userProductColor = color.value;
@@ -83,12 +78,14 @@ function addToCart() {
       let userProductPrice = article.price;
       let userProductImgSrc = article.imageUrl;
       let userProductImgAlt = article.altTxt;
+      let userProductName = article.name
       let userArticleId = userProductId + userProductColor;
+      
 
       // Création d'un objet produit
-
       let userProductArray = {
         ArticleId: userArticleId,
+        Name: userProductName,
         Id: userProductId,
         Color: userProductColor,
         Qty: userProductQty,
@@ -97,19 +94,65 @@ function addToCart() {
         ImgAlt: userProductImgAlt,
       };
       console.log(userProductArray)
-    
-      // Mise à disposition du localStorage si existant
 
+      // Déclaration de la variable dans laquelle on met les keys et les values dans localstorage
       let productLocalStorage = JSON.parse(localStorage.getItem("Product"));
         console.log(productLocalStorage)
-        // On l'enregistre dans le localStorage
 
-        if (productLocalStorage === null)
-          
+      // Comportement si il n'y a pas de produit dans localStorage
+      if (productLocalStorage == null) {
         productLocalStorage = [];
         productLocalStorage.push(userProductArray);
         localStorage.setItem("Product",JSON.stringify(productLocalStorage));
+        //alert("C'est cool, le produit est enregistré");
+      }
+
+      // Comportement si il y a un produit dans localStorage
+      else if (productLocalStorage != null) {
+        productLocalStorage.push(userProductArray);
+        localStorage.setItem("Product", JSON.stringify(productLocalStorage));
+      } console.log(productLocalStorage)
+      
+      /*let mappingProducts = productLocalStorage.find(
+        (el) => el.Id === userProductId && el.Color === userProductColor);
+
+      // Si la condition est vraie on additionne la quantité de l'objet du localStorage qui répond à la condition avec celle de la page en cours et on renvoie le tout au localStorage
+      if (mappingProducts) {
+        
+        // On incrémente la quantité
+        newQty = parseInt(mappingProducts.Qty) + parseInt(userProductQty);
+        mappingProducts.Qty = newQty;
+
+        // On l'enregistre dans le localStorage
+        localStorage.setItem("Product", JSON.stringify(productLocalStorage));
         alert("C'est cool, le produit est enregistré");
+      } else {
+        
+        // Dans tous les autres cas, on enregistre un nouvel objet dans le localStorage
+        productLocalStorage.push(userProductArray);
+        localStorage.setItem("Product", JSON.stringify(productLocalStorage));
+        //alert("C'est cool, le produit est enregistré");
+      }
+      
+      //Incrémentation des quantités, des produits identiques
+      for (let i = 0; i < productLocalStorage.length; i++){
+        if ((userProductArray.Color === productLocalStorage[i].Color) && (userProductArray.Id === productLocalStorage[i].Id)){
+          
+          productLocalStorage[i].Qty += parseInt(userProductArray.Qty);
+          console.log(userProductArray.Qty)
+
+          localStorage.setItem('Product',JSON.stringify(productLocalStorage))
+        }
+      } console.log(productLocalStorage.length)
+
+      let check = productLocalStorage.some( e => e.Id === userProductArray.Id && e.Color === userProductArray.Color)
+      console.log(check)
+      console.log(productLocalStorage)
+    
+      if(!check){
+        productLocalStorage.push(userProductArray)
+        localStorage.setItem('Product', JSON.stringify(productLocalStorage))
+      }*/
     }
   });
 }
